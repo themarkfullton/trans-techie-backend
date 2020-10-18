6fae98c710ef51789bec69ffaf1ffaa9f9a26fac
Mark <the.mark.fullton@gmail.com>
2020-10-18 12:13:34 -0700

    Formatted Blog files

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,13 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from blog.models import BlogPost
 from blog.serializers import BlogPostSerializer
 
+
 class BlogPostListView(ListAPIView):
     queryset = BlogPost.objects.order_by('-date_created')
     serializer_class = BlogPostSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny,)
+
 
 class BlogPostDetailView(RetrieveAPIView):
     queryset = BlogPost.objects.order_by('-date_created')
@@ -17,11 +19,13 @@ class BlogPostDetailView(RetrieveAPIView):
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny,)
 
+
 class BlogPostFeaturedView(ListAPIView):
     queryset = BlogPost.objects.all().filter(featured=True)
     serializer_class = BlogPostSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny,)
+
 
 class BlogPostCategoryView(APIView):
     serializer_class = BlogPostSerializer
