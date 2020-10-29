6f8edc6b7b0cb63fc34fa630a4a1c533d4bea2d9
Mark <the.mark.fullton@gmail.com>
2020-10-29 16:18:54 -0700

    Deploying to Heroku changed static root to staticfiles

--- trans_techie/settings.py
+++ trans_techie/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'build/static')
+#]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL='/media/'
