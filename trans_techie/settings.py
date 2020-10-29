69b7197a6bdadbddf199a408e99f5cee3cbec444
Mark <the.mark.fullton@gmail.com>
2020-10-29 14:26:14 -0700

    Added Amazon S3 hook up

--- trans_techie/settings.py
+++ trans_techie/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_summernote',
     'dotenv',
-    'blog'
+    'blog',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -154,3 +155,12 @@ SUMMERNOTE_THEME = 'bs4'
 CORS_ORIGIN_ALLOW_ALL = True
 
 FILE_UPLOAD_PERMISSIONS = 0o640
+
+AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
